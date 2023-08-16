4723e96fb5c5d258c8995027ff5c1ec816654c39
novinxy <grzesiekn1001@gmail.com>
2023-08-16 10:31:04 +0200

    split tests

--- tests/test_for_loop.py
+++ tests/test_for_loop.py
@@ -1,25 +1,6 @@
 import json2py
 
 
-def test_simple_wile_loop(multiline):
-    # arrange
-    js_code = """
-    while (true) {
-        var i = 10;
-    }
-    """
-
-    # act
-    result = json2py.translate_code(js_code)
-
-    # assert
-    expected_py = """
-    while True:
-        i = 10
-    """
-    assert result == multiline(expected_py)
-
-
 def test_for_in_loop(multiline):
     # arrange
     js_code = """
@@ -58,7 +39,6 @@ def test_for_of_loop(multiline):
     assert result == multiline(expected_py)
 
 
-
 def test_for_loop_ascending(multiline):
     # arrange
     js_code = """
@@ -77,7 +57,7 @@ def test_for_loop_ascending(multiline):
     """
     assert result == multiline(expected_py)
 
- 
+
 def test_for_loop_descending(multiline):
     # arrange
     js_code = """
---
+++ tests/test_while_loop.py
@@ -0,0 +1,20 @@
+import json2py
+
+
+def test_simple_wile_loop(multiline):
+    # arrange
+    js_code = """
+    while (true) {
+        var i = 10;
+    }
+    """
+
+    # act
+    result = json2py.translate_code(js_code)
+
+    # assert
+    expected_py = """
+    while True:
+        i = 10
+    """
+    assert result == multiline(expected_py)
